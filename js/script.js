b7e4b9d1bb216370e10716d2987bd9ead0afbdf5
Michał Lisowiec <miclisowiec@gmail.com>
2021-08-18 16:57:15 +0200

    Remove console.logs

--- js/script.js
+++ js/script.js
@@ -7,19 +7,13 @@ buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); }
 
 function buttonClicked(buttonName) {
   clearMessages();
-  console.log(buttonName + ' został kliknięty');
-  
   const playerMove = buttonName;
-  console.log('ruch gracza to: ' + playerMove);
   const randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('wylosowana liczba to: ' + randomNumber);
   const computerMove = getMoveName(randomNumber);
-  console.log('ruch komputera to: ' + computerMove);
   displayResult(playerMove, computerMove);
 }
 
 function getMoveName(argMoveId) {
-    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
     if (argMoveId == 1) {
       return 'kamień';
     } else if (argMoveId == 2) {
@@ -36,7 +30,6 @@ let playerResult = 0;
 let computerResult = 0;
 
 function displayResult(argPlayerMove, argComputerMove) {
-    console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
     if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
       printMessage('Wygrywasz!');
       playerResult++;
